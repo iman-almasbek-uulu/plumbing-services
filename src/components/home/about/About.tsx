9bac5560d97f25fdbdca378ea7d18e5819891cec
user <ularbekkochkorov0@gmail.com>
2024-12-06 20:05:39 +0600

    burgerMenu

--- src/components/home/about/About.tsx
+++ src/components/home/about/About.tsx
@@ -3,7 +3,11 @@ import scss from './About.module.scss';
 const About = () => {
     return (
         <section id={scss.About}>
-            <div className="container"></div>
+            <div className="container">
+                <div className={scss.about}>
+                    
+                </div>
+            </div>
         </section>
     );
 };
