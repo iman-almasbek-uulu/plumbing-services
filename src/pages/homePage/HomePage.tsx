a580a2e6342580a70026019a925a255037a06ec5
iman <iman.almasbekuulu.kk@gmail.com>
2024-12-09 14:06:28 +0600

    added section testimonials

--- src/pages/homePage/HomePage.tsx
+++ src/pages/homePage/HomePage.tsx
@@ -11,7 +11,7 @@ const HomePage = () => {
             <HeroBanner />
             <About/>
             <How_work/>
-            <Services/>
+            {/* <Services/> */}
             <Masters/>
             <Testimonials/>
         </>
